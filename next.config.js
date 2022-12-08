3147a09317432c3297332bf69f9fba9187074a6d
labeneko <labecat@gmail.com>
2022-12-08 23:18:09 +0900

    nextjsの設定ファイルを変更

--- next.config.js
+++ next.config.js
@@ -6,6 +6,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      ...defaultPathMap,
+      "/404.html": { page: "/404" },
+    }
+  },
 }
 
 module.exports = nextConfig
