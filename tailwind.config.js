f66b8b8470204843da774622b49cd53cbde5b6e6
labeneko <labecat@gmail.com>
2022-12-10 00:26:08 +0900

    2022冬のページを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        winter: "url('bg.jpg')",
+      }),
+    },
   },
   plugins: [],
 }
